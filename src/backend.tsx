f5cce04c389a07bec53c9a35797fd76917bffcc4
Daniel <iam@w-b.dev>
2024-10-27 01:59:38 -0300

    restore external image folder

--- src/backend.tsx
+++ src/backend.tsx
@@ -5,70 +5,70 @@ export const menuItems: MenuItemInterface[] = [
         name: 'Burger',
         description: 'A delicious beef burger',
         price: 8.99,
-        image: 'images/burger.jpg',
+        image: 'exported_images/burger.jpg',
         category: ['Main', 'Lunch']
     },
     {
         name: 'Pizza',
         description: 'A cheesy pizza with toppings',
         price: 12.99,
-        image: 'images/pizza.jpg',
+        image: 'exported_images/pizza.jpg',
         category: ['Main', 'Dinner']
     },
     {
         name: 'Salad',
         description: 'A healthy green salad',
         price: 6.99,
-        image: 'images/salad.jpg',
+        image: 'exported_images/salad.jpg',
         category: ['Starter', 'Lunch']
     },
     {
         name: 'Ice Cream',
         description: 'A sweet treat',
         price: 2.99,
-        image: 'images/ice_cream.jpg',
+        image: 'exported_images/ice_cream.jpg',
         category: ['Dessert']
     },
     {
         name: 'Coffee',
         description: 'A hot cup of coffee',
         price: 1.99,
-        image: 'images/soda.jpg',
+        image: 'exported_images/soda.jpg',
         category: ['Drinks']
     },
     {
         name: 'Fresh Juice',
         description: 'A refreshing cup of tea',
         price: 1.49,
-        image: 'images/juices.jpg',
+        image: 'exported_images/juices.jpg',
         category: ['Drinks']
     },
     {
         name: 'Pasta',
         description: 'A classic pasta dish',
         price: 9.99,
-        image: 'images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
+        image: 'exported_images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
         category: ['Main', 'Dinner']
     },
     {
         name: 'Soup',
         description: 'A hearty bowl of soup',
         price: 4.99,
-        image: 'images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
+        image: 'exported_images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
         category: ['Starter', 'Lunch']
     },
     {
         name: 'Cake',
         description: 'A slice of cake',
         price: 3.99,
-        image: 'images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
+        image: 'exported_images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
         category: ['Dessert']
     },
     {
         name: 'Beer',
         description: 'A cold beer',
         price: 3.49,
-        image: 'images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
+        image: 'exported_images/Gemini_Generated_Image_95hklh95hklh95hk.jpg',
         category: ['Drinks']
     },
 ];
