b84ee953318221740bd2d04bea7d24cce3794503
Daniel <iam@w-b.dev>
2024-10-28 12:04:47 -0300

    Minor Chrome warning fixes

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -1,10 +1,10 @@
-const isLocalhost = Boolean(
-    window.location.hostname === 'localhost' ||
-    window.location.hostname === '[::1]' ||
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+// const isLocalhost = Boolean(
+//     window.location.hostname === 'localhost' ||
+//     window.location.hostname === '[::1]' ||
+//     window.location.hostname.match(
+//         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+//     )
+// );
 
 type Config = {
     onSuccess?: (registration: ServiceWorkerRegistration) => void;
