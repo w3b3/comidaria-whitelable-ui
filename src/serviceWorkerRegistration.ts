2bf27581659b31c77151ff37501ae0cce49d1fbf
Daniel <iam@w-b.dev>
2024-10-28 11:52:46 -0300

    Updates to service worker path due to Chrome console warnings
    Error during service worker registration: SecurityError: Failed to register a ServiceWorker for scope ('https://emitajuba.com.br/') with script ('https://emitajuba.com.br/service-worker.js'): The script has an unsupported MIME type ('text/html').

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -13,13 +13,14 @@ type Config = {
 
 export function register(config?: Config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-        if (publicUrl.origin !== window.location.origin) {
+        // const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+        const swUrl = '/service-worker.js'; // Hardcoded path to service worker script
+        if (swUrl.origin !== window.location.origin) {
             return;
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+            // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
             if (isLocalhost) {
                 checkValidServiceWorker(swUrl, config);
