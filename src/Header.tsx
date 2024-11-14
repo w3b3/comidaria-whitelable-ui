f493572e2c58045a8b8eeedde197b2fc19b0fe85
Daniel <iam@w-b.dev>
2024-11-13 23:52:56 -0300

    simplify

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
 import { useRestaurants } from "./useRestaurants";
 
 const HeaderComponent = () => {
-    const [selectedRestaurant, setSelectedRestaurant] = useState<string | null>(null);
-    const { data: restaurants, isLoading } = useRestaurants();
-    return <div>
-        <h1>Comidaria</h1>
-        <pre>{restaurants?.length}</pre>
-    </div>;
-  }
+  const { data: restaurants, isLoading } = useRestaurants();
+  return (
+    <div>
+      <h1>Comidaria</h1>
+      {/* hide the following component if the query did not load */}
+      {isLoading ? <h2>Loading...</h2> : <h2>❌ {restaurants?.length} ❌</h2>}
+    </div>
+  );
+};
 
 export { HeaderComponent };
