29e84557c4d26cfe70a54e10b7f9268795d39829
Daniel <iam@w-b.dev>
2024-11-14 10:28:16 -0300

    apply formatting

--- src/useMenuItems.ts
+++ src/useMenuItems.ts
@@ -1,12 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const fetchMenuItems = async (restaurant_name: string) => {
-  const { data } = await axios.get(`https://api.emitajuba.com.br/data/${restaurant_name}`);
+  const { data } = await axios.get(
+    `https://api.emitajuba.com.br/data/${restaurant_name}`
+  );
   // can I type this response as MenuItemInterface? How to handle the possibility of errors?
   return data;
 };
 
 export const useMenuItems = (restaurant_name: string) => {
-  return useQuery({ queryKey: ['menuItems', restaurant_name], queryFn: () => fetchMenuItems(restaurant_name) });
+  return useQuery({
+    queryKey: ["menuItems", restaurant_name],
+    queryFn: () => fetchMenuItems(restaurant_name),
+  });
 };
